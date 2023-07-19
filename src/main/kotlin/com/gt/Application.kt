06afc22642f6a8aa54ac5234213c644852c37160
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2023-07-19 08:12:22 -0400

    simplify and make shared session discoverable

--- src/main/kotlin/com/gt/Application.kt
+++ src/main/kotlin/com/gt/Application.kt
@@ -1,6 +1,5 @@
 package com.gt
 
-import com.gt.matlab.matlabEngine
 import com.gt.plugins.configureAdministration
 import com.gt.plugins.configureMonitoring
 import com.gt.plugins.configureRouting
@@ -8,26 +7,16 @@ import com.gt.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import kotlinx.coroutines.launch
 
 
 fun main(vararg args: String) {
-    val port = args.firstOrNull()?.toInt() ?: 8080
-
-    embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 6081, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
-    initMatlabEngine()
     configureMonitoring()
     configureSerialization()
     configureAdministration()
     configureRouting()
-}
-
-private fun Application.initMatlabEngine() = launch {
-    val matlabOutput = matlabEngine.eval("ver")
-    log.info("Successfully connected to Matlab :")
-    log.info(matlabOutput.output)
 }
--- src/main/kotlin/com/gt/matlab/MatlabApi.kt
+++ src/main/kotlin/com/gt/matlab/MatlabApi.kt
@@ -1,19 +1,58 @@
 package com.gt.matlab
 
+import com.mathworks.engine.MatlabEngine
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
+import java.io.StringWriter
 
 fun Route.matlabApiRouting() {
 
-    post("/eval") {
-        val command = call.receive<EvalCommandRequest>().command
-        val output = matlabEngine.eval(command)
-        call.respond(EvalCommandResponse(command, output))
+    route("matlab") {
+
+        get("shared-sessions") {
+            call.respond(MatlabEngine.findMatlab())
+        }
+
+        route("{session-id}") {
+
+            post("/eval") {
+                val sessionId = call.parameters["session-id"]!!
+                val command = call.receive<EvalCommandRequest>().command
+
+                val output = useEngine(sessionId) { eval(command) }
+
+                call.respond(EvalCommandResponse(command, output))
+            }
+
+        }
+
     }
 
+}
+
+class Matlab(sessionId: String) {
+
+    private val engine = MatlabEngine.connectMatlab(sessionId)
+
+    fun disconnect() = engine.disconnect()
+
+    fun eval(command: String): MatlabOutput {
+        val outputStream = StringWriter()
+        val errorStream = StringWriter()
+        engine.eval(command, outputStream, errorStream)
+        return MatlabOutput(outputStream.toString(), errorStream.toString())
+    }
+
+}
+
+fun useEngine(sessionId: String, bloc: Matlab.() -> MatlabOutput): MatlabOutput {
+    val matlab = Matlab(sessionId)
+    val result = matlab.bloc()
+    matlab.disconnect()
+    return result
 }
 
 @Serializable
